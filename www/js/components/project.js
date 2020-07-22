5dac12ed94a20b8ae8e6d13c7d0f97b86218154d
codeallthethingz <howdoicontactwill@gmail.com>
2020-07-22 10:55:09 -0700

    commented line

--- www/js/components/project.js
+++ www/js/components/project.js
@@ -16,9 +16,10 @@ let Project = {
         return [
             m('h1', { class: 'bg-gray-900 p-2' }, GS.model.currentProject.name),
             m('div', { class: 'flex' }, [
-                m('iframe', { class: 'flex-1', src: 'https://stats.kube.searchspring.io/d-solo/nshAi8pmz/jmx-exporter-prometheus?refresh=30s&orgId=1&from=1590286296335&to=1590289896335&var-job=indexer-west&var-pod=All&panelId=8', width: '600px', height: '400px' }),
-                m('iframe', { class: 'flex-1', src: 'https://service.us2.sumologic.com/ui/dashboard.html?k=L9QWiaqGyHhljC9OdCgNs2RHOMrqqaCpgbbil6QsqPNyCZg7koQNFc93UMlG&f=&t=r', width: '600px', height: '400px' })
-            ])
+                // m('iframe', { class: 'flex-1', src: 'https://stats.kube.searchspring.io/d-solo/nshAi8pmz/jmx-exporter-prometheus?refresh=30s&orgId=1&from=1590286296335&to=1590289896335&var-job=indexer-west&var-pod=All&panelId=8', width: '600px', height: '400px' }),
+                // m('iframe', { class: 'flex-1', src: 'https://service.us2.sumologic.com/ui/dashboard.html?k=L9QWiaqGyHhljC9OdCgNs2RHOMrqqaCpgbbil6QsqPNyCZg7koQNFc93UMlG&f=&t=r', width: '600px', height: '400px' })
+            ]
+            )
         ]
     }
 }
